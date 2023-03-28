1805d54d0e646eab422d43a90dc7cdbfc39c89b0
“Daniela <su>
2023-03-28 14:11:01 -0300

    completo el tp1_1

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,19 @@
 #include<stdlib.h>
 
 int main(){
-    printf("Hola Mundo");
-}
+    printf("Hola Mundo \n\n");
+
+    int *punt;
+    int valor;
+
+    valor = 10;
+    punt = &valor;
+
+    printf("Contenido del puntero: %p  \n\n", punt); 
+    printf("Direccion de memoria almacenada por el puntero:%p \n\n", &punt); 
+    printf("Direccion de memoria de la variable:%p \n\n", &punt); 
+    printf("Direccion de memoria del puntero:%p  \n\n", &punt);  
+    printf("Tamanio de memoria utilizado por esa variable usando la funcion sizeof():'%d'  \n\n", (sizeof( int )));
+
+    return 0;
+} 
